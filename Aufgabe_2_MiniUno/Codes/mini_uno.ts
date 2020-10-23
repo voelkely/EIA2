f71344a5f5034c435eb75e2b6d1efa2d36bc0c78
voelkely <yvonne@voelkel24.com>
2020-10-23 15:42:09 +0200

    up

--- Aufgabe_2_MiniUno/Codes/mini_uno.ts
+++ Aufgabe_2_MiniUno/Codes/mini_uno.ts
@@ -1,3 +1,21 @@
-function verteiler() {
-  alert("Wie viele Karten sollen verteilt werden?");
+namespace Mini Uno {
+
+    interface Card {
+        farbe: string;
+        zahl: number;
+        bild:string;
+
+    }
+
+}
+
+let farbe: string []: ["Rot", "Gelb", "Blau", "Grün"];
+let zahl: number []: ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var handcards: string[]: [];
+
+//variablen
+let gelb1: Cards= {
+    zahl: 1,
+    farbe: "rot",
+    bild: "img/rot1.jpg"
 }
--- Aufgabe_2_MiniUno/Codes/mini_uno_js.js
+++ Aufgabe_2_MiniUno/Codes/mini_uno_js.js
@@ -1,1 +1,67 @@
 //Karten definieren
+let rot1 = {
+    farbe: "rot",
+    zahl: 1,
+    Bild: "img/rot1.jpg"
+};
+let rot2 = {
+    farbe: "rot",
+    zahl: 2,
+    Bild: "img/rot2.jpg"
+};
+let rot3 = {
+    farbe: "rot",
+    zahl: 3,
+    Bild: "img/rot3.jpg"
+};
+let rot4 = {
+    farbe: "rot",
+    zahl: 4,
+    Bild: "img/rot4.jpg"
+};
+let rot5 = {
+    farbe: "rot",
+    zahl: 5,
+    Bild: "img/rot5.jpg"
+};
+let rot6 = {
+    farbe: "rot",
+    zahl: 6,
+    Bild: "img/rot6.jpg"
+};
+let rot7 = {
+    farbe: "rot",
+    zahl: 7,
+    Bild: "img/rot7.jpg"
+};
+let rot8 = {
+    farbe: "rot",
+    zahl: 8,
+    Bild: "img/rot8.jpg"
+};
+let rot9 = {
+    farbe: "rot",
+    zahl: 9,
+    Bild: "img/rot9.jpg"
+};
+
+let blau1 = {
+    farbe: "blau",
+    zahl: 1,
+    Bild: "img/blau1.jpg"
+};
+let blau2 = {
+    farbe: "blau",
+    zahl: 2,
+    Bild: "img/blau2.jpg"
+};
+let blau3 = {
+    farbe: "blau",
+    zahl: 3,
+    Bild: "img/blau3.jpg"
+};
+let blau4 = {
+    farbe: "blau",
+    zahl: 4,
+    Bild: "img/blau4.jpg"
+};
