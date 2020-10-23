0123b0b0439bcc86a3700dad7a10d9e0192ca820
voelkely <yvonne@voelkel24.com>
2020-10-23 14:58:59 +0200

    up

--- Mini Uno/mini_uno_ts.ts
+++
@@ -1,7 +0,0 @@
-var i: string = prompt("Wie viele Karten sollen ausgeteilt werden?");
-    var counter: number = Number(i);
-    // GENERELLE FOR SCHLEIFE DIE u-mal DURCHLAEUFT
-    for (var u: number = 0; u < counter; u++) {
-        var card: string = "";
-        var r_color: number = randomBetween(1, 1000);
-        console.log(r_color);
