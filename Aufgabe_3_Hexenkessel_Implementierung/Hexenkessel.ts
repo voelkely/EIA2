727ffd35aa9024aaf85781c7c1c5c55e66793aec
voelkely <yvonne@voelkel24.com>
2020-10-30 15:02:00 +0100

    update

--- Aufgabe_3_Hexenkessel_Implementierung/Hexenkessel.ts
+++ Aufgabe_3_Hexenkessel_Implementierung/Hexenkessel.ts
@@ -7,26 +7,31 @@ namespace L03_Hexenkessel {
         let slider: HTMLInputElement = <HTMLInputElement>document.querySelector("input#amount");
 
         form.addEventListener("change", handleChange);
-        slider.addEventListener("input", displayAmount);
     }
 
     function handleChange(_event: Event): void {
-        // console.log(_event);
-        // let drink: HTMLSelectElement = <HTMLSelectElement>document.querySelector("select");
-        // console.log(drink.value);
+        console.log(_event);
+        let effect: HTMLSelectElement = <HTMLSelectElement>document.querySelector("select");
+        console.log(effect.value);
 
-        // let inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll("input");
-        // console.log(inputs);
+        let inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll("input");
+        console.log(inputs);
 
-        let order: HTMLDivElement = <HTMLDivElement>document.querySelector("div#order");
-        order.innerHTML = "";
+        let potion: HTMLDivElement = <HTMLDivElement>document.querySelector("div#potion");
+        potion.innerHTML = "";
 
         let formData: FormData = new FormData(document.forms[0]);
         for (let entry of formData) {
             let item: HTMLInputElement = <HTMLInputElement>document.querySelector("[value='" +  entry[1] + "']");
             let price: number = Number(item.getAttribute("price"));
 
-            order.innerHTML += item.name + "  € " +  price;
+        if (entry [0] == "Potion")
+            order.innerHTML += entry [1];
+        else 
+            order.innerHTML += item.name;
+        
+        order.innerHTML +=  "  € " +  price;
+        
         }
     }
 
