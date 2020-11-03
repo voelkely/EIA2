e35dc6a7abd31f5808217bee78a3b8f9d2efb1d4
voelkely <yvonne@voelkel24.com>
2020-11-03 16:06:13 +0100

    adding Data.ts

---
+++ Aufgabe_L03_Code/Data.ts
@@ -0,0 +1,12 @@
+namespace L04_Hexenkessel {
+
+    interface: Item {
+        name: string;
+        price: number;
+    }
+
+    inteface: Data {
+        [category: string]: Item [];
+        
+    }
+}
