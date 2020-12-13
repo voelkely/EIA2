71fb214091588a31c66503c3318e4661cc01043a
voelkely <yvonne@voelkel24.com>
2020-12-13 15:49:56 +0100

    update

--- Aufgabe_L08/Aufgabe/SkipisteNEU.ts
+++ Aufgabe_L08/Aufgabe/SkipisteNEU.ts
@@ -293,7 +293,6 @@ namespace L08_Canvas_SkipisteNEU {
     
             crc2.fillStyle = "#5c4411";
             crc2.fill();
-    
             crc2. save();
     
             //Fenster
@@ -301,17 +300,22 @@ namespace L08_Canvas_SkipisteNEU {
             crc2.fillStyle = "#d3e6e6";
             crc2.fillRect(225, 475 , 100, 60);
             crc2. save();
+
+            crc2.fillStyle = "black";
+            crc2.font = "10px sans-serif";
+            crc2.fillText("KASSE SKI LIFT", 230, 485);
             
             //Dach
     
             crc2.beginPath();
-            crc2.moveTo(200, 450); //(300, 250)
-            crc2.lineTo(275, 400); // (375, 175)
-            crc2.lineTo(350, 450); // (450, 240)
+            crc2.moveTo(200, 450); 
+            crc2.lineTo(275, 400); 
+            crc2.lineTo(350, 450); 
             crc2.stroke();
             crc2.closePath();
     
             crc2.lineWidth = 15;
+            crc2.lineCap = "round";
             crc2.strokeStyle = "#d3e6e6";
             crc2.stroke();
     
@@ -320,6 +324,67 @@ namespace L08_Canvas_SkipisteNEU {
     
             crc2.save();  
             crc2.restore(); 
+
+            //Eiszapfen groß
+
+            crc2.beginPath();
+            crc2.moveTo(205, 456);
+            crc2.lineTo(210, 525);
+            crc2.lineTo(215, 456);
+            crc2.lineWidth = 2;
+
+            crc2.stroke();
+            crc2.closePath();
+    
+            crc2.fillStyle = "white";
+            crc2.fill();
+
+             //Eiszapfen klein
+
+            crc2.beginPath();
+            crc2.moveTo(220, 456);
+            crc2.lineTo(222, 500);
+            crc2.lineTo(225, 456);
+            crc2.lineWidth = 2;
+ 
+            crc2.stroke();
+            crc2.closePath();
+     
+            crc2.fillStyle = "white";
+            crc2.fill();
+
+            //Eiszapfen klein rechts
+
+            crc2.beginPath();
+            crc2.moveTo(330, 456);
+            crc2.lineTo(333, 500);
+            crc2.lineTo(336, 456);
+            crc2.lineWidth = 2;
+  
+            crc2.stroke();
+            crc2.closePath();
+      
+            crc2.fillStyle = "white";
+            crc2.fill();
+
+            //Schneehaufen an der Hütte
+
+            crc2.beginPath();
+            crc2.arc(355, 600, 15, 0, 2 * Math.PI);
+            crc2.arc(325, 600, 15, 0, 2 * Math.PI);
+            crc2.arc(300, 600, 15, 0, 2 * Math.PI);
+            crc2.arc(290, 600, 15, 0, 2 * Math.PI);
+            crc2.arc(275, 600, 15, 0, 2 * Math.PI);
+            crc2.arc(255, 600, 15, 0, 2 * Math.PI);
+            crc2.arc(235, 600, 15, 0, 2 * Math.PI);
+            crc2.arc(217, 600, 15, 0, 2 * Math.PI);
+            crc2.arc(200, 600, 15, 0, 2 * Math.PI);
+            crc2.closePath();
+            crc2.fillStyle = "white";
+            crc2.fill();
+
+            crc2.restore();
+
         
         }
 
